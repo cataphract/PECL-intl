953c7812e2dd6b64449891621a11cb7c76fc0e6b
Ed Batutis <ebatutis@php.net>
2007-10-22 23:57:00 +0000

    added support for normalizer
    git-svn-id: https://svn.php.net/repository/pecl/intl/trunk@244651 c90b9560-bf6c-de11-be94-00142212c4b1

--- php_intl.c
+++ php_intl.c
@@ -40,6 +40,11 @@
 #include "formatter/formatter_main.h"
 #include "formatter/formatter_parse.h"
 
+#include "normalizer/normalizer.h"
+#include "normalizer/normalizer_class.h"
+#include "normalizer/normalizer_create.h"
+#include "normalizer/normalizer_normalize.h"
+
 #include "locale/locale.h"
 #include "locale/locale_class.h"
 #include "locale/locale_methods.h"
@@ -135,6 +140,13 @@ ZEND_END_ARG_INFO()
 
 #define intl_0_args collator_static_0_args
 #define intl_1_arg collator_static_1_arg
+
+ZEND_BEGIN_ARG_INFO_EX( normalizer_3_args, 0, 0, 3 )
+	ZEND_ARG_INFO( 0, arg1 )
+	ZEND_ARG_INFO( 0, arg2 )
+	ZEND_ARG_INFO( 0, arg3 )
+ZEND_END_ARG_INFO()
+
 /* }}} */
 
 /* {{{ intl_functions[]
@@ -177,21 +189,24 @@ zend_function_entry intl_functions[] = {
 	PHP_FE( numfmt_get_error_code, NULL )
 	PHP_FE( numfmt_get_error_message, NULL )
 
-	//Locale functions
-    PHP_NAMED_FE( locale_get_default, zif_locale_get_default, locale_0_args )
-    PHP_NAMED_FE( locale_set_default, zif_locale_set_default, locale_1_arg )
-    PHP_FE( locale_get_primary_language, locale_1_arg )
-    PHP_FE( locale_get_script, locale_1_arg )
-    PHP_FE( locale_get_region, locale_1_arg )
-    PHP_FE( locale_get_variant, locale_1_arg )
-    PHP_FE( locale_get_keywords, locale_1_arg )
-    PHP_FE( locale_get_display_script, locale_2_args )
-    PHP_FE( locale_get_display_region, locale_2_args )
-    PHP_FE( locale_get_display_name, locale_2_args )
-    PHP_FE( locale_get_display_language, locale_2_args)
-    PHP_FE( locale_get_display_variant, locale_2_args )
+	// normalizer functions
+	PHP_FE( normalizer_normalize, normalizer_3_args )
+	PHP_FE( normalizer_is_normalized, normalizer_3_args )
 
-	
+	//Locale functions
+	PHP_NAMED_FE( locale_get_default, zif_locale_get_default, locale_0_args )
+	PHP_NAMED_FE( locale_set_default, zif_locale_set_default, locale_1_arg )
+	PHP_FE( locale_get_primary_language, locale_1_arg )
+	PHP_FE( locale_get_script, locale_1_arg )
+	PHP_FE( locale_get_region, locale_1_arg )
+	PHP_FE( locale_get_variant, locale_1_arg )
+	PHP_FE( locale_get_keywords, locale_1_arg )
+	PHP_FE( locale_get_display_script, locale_2_args )
+	PHP_FE( locale_get_display_region, locale_2_args )
+	PHP_FE( locale_get_display_name, locale_2_args )
+	PHP_FE( locale_get_display_language, locale_2_args)
+	PHP_FE( locale_get_display_variant, locale_2_args )
+
 	// common functions
 	PHP_FE( intl_get_error_code, intl_0_args )
 	PHP_FE( intl_get_error_message, intl_0_args )
@@ -253,11 +268,17 @@ PHP_MINIT_FUNCTION( intl )
 	// Expose NumberFormatter constants to PHP scripts
 	formatter_register_constants( INIT_FUNC_ARGS_PASSTHRU );
 
-    // Register 'Locale' PHP class
-    locale_register_Locale_class( TSRMLS_C );
+	// Register 'Normalizer' PHP class
+	normalizer_register_Normalizer_class( TSRMLS_C );
 
-    // Expose Locale constants to PHP scripts
-    locale_register_constants( INIT_FUNC_ARGS_PASSTHRU );
+	// Expose Normalizer constants to PHP scripts
+	normalizer_register_constants( INIT_FUNC_ARGS_PASSTHRU );
+	
+	// Register 'Locale' PHP class
+	locale_register_Locale_class( TSRMLS_C );
+
+	// Expose Locale constants to PHP scripts
+	locale_register_constants( INIT_FUNC_ARGS_PASSTHRU );
 
 	// Expose ICU error codes to PHP scripts.
 	intl_expose_icu_error_codes( INIT_FUNC_ARGS_PASSTHRU );
@@ -292,9 +313,9 @@ PHP_RSHUTDOWN_FUNCTION( intl )
 	if(INTL_G(current_collator)) {
 		INTL_G(current_collator) = NULL;
 	}
-    if(INTL_G(current_locale)) {
-        INTL_G(current_locale) = NULL;
-    }
+	if(INTL_G(current_locale)) {
+		INTL_G(current_locale) = NULL;
+	}
 	intl_error_reset( NULL TSRMLS_CC);
 	return SUCCESS;
 }
