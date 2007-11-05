364d41da29e84b1450e45cf809e9b47f318f4949
Ed Batutis <ebatutis@php.net>
2007-11-05 20:13:18 +0000

    update doc formatting to standard
    git-svn-id: https://svn.php.net/repository/pecl/intl/trunk@245646 c90b9560-bf6c-de11-be94-00142212c4b1

--- doc/normalizer_api.php
+++ doc/normalizer_api.php
@@ -1,4 +1,5 @@
 <?php
+
 #############################################################################
 # Object-oriented API
 #############################################################################
@@ -25,46 +26,46 @@ class Normalizer {
 #############################################################################
 
 	/**
-     * Valid normalization form values.
-     *
-     * @see Normalizer::normalize()
-     * @see Normalizer::isNormalize()
-     * @see normalizer_normalize()
-     * @see normalizer_is_normalized()
-     */
-   const FORM_C  = 'UNORM_FORM_C';
-   const FORM_D  = 'UNORM_FORM_D';
-   const FORM_KC = 'UNORM_FORM_KC';
-   const FORM_KD = 'UNORM_FORM_KD';
-   const NONE    = 'UNORM_NONE';
+	 * Valid normalization form values.
+	 *
+	 * @see Normalizer::normalize()
+	 * @see Normalizer::isNormalize()
+	 * @see normalizer_normalize()
+	 * @see normalizer_is_normalized()
+	*/
+	const FORM_C  = 'UNORM_FORM_C';
+	const FORM_D  = 'UNORM_FORM_D';
+	const FORM_KC = 'UNORM_FORM_KC';
+	const FORM_KD = 'UNORM_FORM_KD';
+	const NONE    = 'UNORM_NONE';
 
 	/**
-     * Valid normalizer options.
-     *
-     * @see Normalizer::normalize()
-     * @see Normalizer::isNormalize()
-     * @see normalizer_normalize()
-     * @see normalizer_is_normalized()
-     */
-   const OPTION_DEFAULT = 'OPTION_DEFAULT';
+	 * Valid normalizer options.
+	 *
+	 * @see Normalizer::normalize()
+	 * @see Normalizer::isNormalize()
+	 * @see normalizer_normalize()
+	 * @see normalizer_is_normalized()
+	*/
+	const OPTION_DEFAULT = 'OPTION_DEFAULT';
 
-   /**
-    * Normalizes the input provided and returns the normalized string
-    * @param  string     $input    The input string to normalize
-    * @param  [string]   $form     One of the normalization forms
-    * @param  [array]    $options  Normalization options
-    * @return string               The normalized string or null if an error occurred.
-    */
-   public static function normalize($input, $form = Normalizer::FORM_C, $options = null) {}
+	/**
+	 * Normalizes the input provided and returns the normalized string
+	 * @param   string     $input    The input string to normalize
+	 * @param   [string]   $form     One of the normalization forms
+	 * @param   [array]    $options  Normalization options
+	 * @return  string               The normalized string or null if an error occurred.
+	*/
+	public static function normalize($input, $form = Normalizer::FORM_C, $options = null) {}
 
-   /**
-    * Checks if the provided string is already in the specified normalization form.
-    * @param  string    $input    The input string to normalize
-    * @param  [string]  $form     One of the normalization forms
-    * @param  [array]   $options  Normalization options
-    * @return boolean             True if normalized, false otherwise or if there is an error
-    */
-   public static function isNormalized($input, $form = Normalizer::FORM_C, $options = null) {}
+	/**
+	 * Checks if the provided string is already in the specified normalization form.
+	 * @param  string    $input    The input string to normalize
+	 * @param  [string]  $form     One of the normalization forms
+	 * @param  [array]   $options  Normalization options
+	 * @return boolean             True if normalized, false otherwise or if there is an error
+	*/
+	public static function isNormalized($input, $form = Normalizer::FORM_C, $options = null) {}
 
 }
 
@@ -72,23 +73,23 @@ class Normalizer {
 # Procedural API
 #############################################################################
 
-/**
- * Normalizes the input provided and returns the normalized string
- * @param  string    $input    The input string to normalize
- * @param  [string]  $form     One of the normalization forms
- * @param  [array]   $options  Normalization options
- * @return string              The normalized string or null if an error occurred.
- */
-function normalizer_normalize($input, $form = Normalizer::FORM_C, $options = null) {}
+	/**
+	 * Normalizes the input provided and returns the normalized string
+	 * @param  string    $input    The input string to normalize
+	 * @param  [string]  $form     One of the normalization forms
+	 * @param  [array]   $options  Normalization options
+	 * @return string              The normalized string or null if an error occurred.
+	*/
+	function normalizer_normalize($input, $form = Normalizer::FORM_C, $options = null) {}
 
-/**
- * Checks if the provided string is already in the specified normalization form.
- * @param  string    $input    The input string to normalize
- * @param  [string]  $form     One of the normalization forms
- * @param  [array]   $options  Normalization options
- * @return boolean             True if normalized, false otherwise or if there an error
- */
-function normalizer_is_normalized($input, $form = Normalizer::FORM_C, $options = null) {}
+	/**
+	 * Checks if the provided string is already in the specified normalization form.
+	 * @param  string    $input    The input string to normalize
+	 * @param  [string]  $form     One of the normalization forms
+	 * @param  [array]   $options  Normalization options
+	 * @return boolean             True if normalized, false otherwise or if there an error
+	*/
+	function normalizer_is_normalized($input, $form = Normalizer::FORM_C, $options = null) {}
 
 
 ?>
