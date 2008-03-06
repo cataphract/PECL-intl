48b71f0f6f821d726e8f6e3be3543ad6307f455c
Kirti Velankar <kirtig@php.net>
2008-03-06 00:24:39 +0000

    Added procedural API
    git-svn-id: https://svn.php.net/repository/pecl/intl/trunk@254337 c90b9560-bf6c-de11-be94-00142212c4b1

--- doc/datefmt_api.php
+++ doc/datefmt_api.php
@@ -18,8 +18,14 @@
  *  echo $formatter->format(time());
  * </code>
  *
+ * @see http://www.icu-project.org/apiref/icu4c/udat_8h.html
+ *
  */
 class DateFormatter {
+
+#############################################################################
+# Common constants.
+#############################################################################
 
    /**
     * The following constants are used to specify different formats
@@ -38,8 +44,11 @@ class DateFormatter {
      * Examples might include locale="hi@calendar=BUDDHIST"
      */
     const TRADITIONAL = 0; // non-Gregorian calendar that is locale-defined, required by ICU
-    const GREGORIAN = 1; // Gregorian calendar
+    const GREGORIAN = 1 ;// Gregorian calendar
 	
+#############################################################################
+# Object-oriented API
+#############################################################################
 	/**
 	 * Create a date formatter 
 	 *
@@ -49,11 +58,12 @@ class DateFormatter {
 	 * @param [String] $timezone  Time zone ID ; default is system default
 	 * @param [integer] $calendar Calendar to use for formatting or parsing; default is
 	 *                            GREGORIAN
+	 * @param [string]  $pattern  Optional pattern to use when formatting or parsing
 	 * @return DateFormatter
 	 * @see __construct
 	 * @see datefmt_create
 	 */
-	public function __construct($locale, $datetype, $timetype, $timezone = null, $calendar= null) {}
+	public function __construct($locale, $datetype, $timetype, $timezone = null, $calendar= null , $pattern= null) {}
 
 	/**
 	 * Create a date formatter 
@@ -64,11 +74,12 @@ class DateFormatter {
 	 * @param [string] $timezone  Time zone ID ; default is system default
 	 * @param [integer] $calendar Calendar to use for formatting or parsing; default is
 	 *                            GREGORIAN
+	 * @param [string]  $pattern  Optional pattern to use when formatting or parsing
 	 * @return DateFormatter
 	 * @see __construct
 	 * @see datefmt_create
 	 */
-	public static function create($locale, $datetype, $timetype, $timezone = null, $calendar= null) {}
+	public static function create($locale, $datetype, $timetype, $timezone = null, $calendar= null , $pattern= null) {}
 
 	/**
          * formats the time value as a string.
@@ -204,6 +215,18 @@ class DateFormatter {
 	 */
          public function getCalendar() {}
 
+        /**
+    	 * @return string the pattern string being used to format/parse 
+	 */
+	public function getPattern() {}
+
+        /**
+         * @param  string $pattern new pattern string to use
+         * @return boolean 'true' if successful, 'false' if an error occured. Bad format
+         *                 strings are usually the cause of the latter.
+         */
+	public function setPattern($pattern) {}
+
 	/**
 	 * Get the error code from last operation
 	 *
@@ -221,7 +244,190 @@ class DateFormatter {
 
 }
 
-/** Now the same as procedural API */
+#############################################################################
+# Procedural API
+#############################################################################
+
+
+	/**
+	 * Create a date formatter 
+	 *
+	 * @param string  $locale     Locale to use when formatting or parsing
+	 * @param integer $datetype   Date type to use (none, short, medium, long, full)
+	 * @param integer $timetype   Time type to use (none, short, medium, long, full)
+	 * @param [string] $timezone  Time zone ID ; default is system default
+	 * @param [integer] $calendar Calendar to use for formatting or parsing; default is
+	 *                            GREGORIAN
+	 * @param [string]  $pattern  Optional pattern to use when formatting or parsing
+	 * @return DateFormatter
+	 * @see datefmt_create
+	 */
+	public static function datefmt_create($locale, $datetype, $timetype, $timezone = null, $calendar= null ,$pattern=null ) {}
+
+	/**
+         * formats the time value as a string.
+         * @param DateFormatter $fmt The date formatter resource
+	 * @param [mixed] $value - value to format
+         *     integer: a unix timestamp value (seconds since epoch, UTC)
+         *     array: a localtime array 
+         * @return string  a formatted string or, if an error occurred, 'null'. 
+	 */
+        public function datefmt_format($fmt , $value) {}
+
+
+	 /**
+	  * converts the string $value to a Unix timestamp (a time_t integer
+	  * value, seconds since epoch).
+	  *
+	  * @param  DateFormatter $formatter The formatter resource
+	  * @param  string  $value   string to convert to a time
+	  * @return integer          timestamp value
+	  */
+	 public function datefmt_parse_to_timestamp($fmt , $value) {}
+
+	 /**
+	  * Field-based equivalent of parse(). 
+	  *
+	  * @param string $value  String value to parse into a date. 
+	  * @return array         An array in the same format as localtime produces
+	  * @see http://www.php.net/manual/en/function.localtime.php
+	  * @see http://www.w3.org/TR/2005/NOTE-timezone-20051013/
+	  */
+         public function datefmt_parse_to_localtime($fmt , $value) {}
+
+        /**
+         * converts string $value to an incremental time value, starting at
+	 * $parse_pos and consuming as much of the input value as possible
+	 * If no error occurs before $value is consumed, $parse_pos will contain -1
+	 * otherwise it will contain the position at which parsing ended (and the error
+	 * occurred). $error will contain the error code. Note that this function
+	 * does not change the value in get_error().
+	 * @param string  $value      string to convert to a time
+	 * @param integer $parse_pos  position at which to start the parsing in $value (zero-based)
+	 *                            This variable will contain the end position if the parse fails
+	 *                            If $parse_pos > strlen($value), the parse fails immediately.
+	 * @param integer $error      error value of any error that occurs during parsing
+	 * @return integer timestamp  parsed value
+	 */
+	 public function datefmt_parse($fmt , $value, $parse_pos, $error) {}
+	 
+        /**
+         * converts string $value to a field-based time value, starting at
+	 * $parse_pos and consuming as much of the input value as possible
+	 * If no error occurs before $value is consumed, $parse_pos will contain -1
+	 * otherwise it will contain the position at which parsing ended (and the error
+	 * occurred). $error will contain the error code. Note that this function
+	 * does not change the value in get_error().
+	 * @param string  $value      string to convert to a time
+	 * @param array   $localtime  the array to store the parsed value; values in this array will
+	 *                            be overwritten!
+	 * @param integer $parse_pos  position at which to start the parsing in $value (zero-based)
+	 *                            This variable will contain the end position if the parse fails
+	 *                            If $parse_pos > strlen($value), the parse fails immediately.
+	 * @param integer $error      error value of any error that occurs during parsing
+	 * @return array localtime compatible array of integers
+	 */
+	 public function datefmt_localtime($fmt , $value, $localtime, $parse_pos, $error) {}
+
+	 /**
+	  * @return integer the current 'datetype' value of the formatter
+	  */
+         public function datefmt_get_datetype($fmt ) {}
+
+	 /**
+	  * @return integer the current 'timetype' value of the formatter
+	  */
+         public function datefmt_get_timetype($fmt) {}
+
+	 /**
+	  * @return boolean   'true' if parser is lenient, 'false' if parser is strict
+	  *                   default value for parser is 'false'.
+	  */
+         public function datefmt_is_lenient($fmt) {}
+
+	 /**
+	  * @param boolean $lenient  sets whether the parser is lenient or not, default is 'false'
+          *                          'true' sets the parser to accept otherwise flawed date or 
+	  *                          time patterns, parsing as much as possible to obtain a value.
+          *                          'false' sets the parser to strictly parse strings into dates. 
+	  *                          Extra space, unrecognized tokens, or invalid values 
+	  *                          ("Feburary 30th") are not accepted.
+	  *
+          * @return boolean          'true' if successful; 'false' if an error occurred. 
+	  */
+	 public function datefmt_set_lenient($fmt , $lenient) {}
+
+
+	 /**
+	  * @param  [integer]  which locale should be returned? 
+	  *                    values may include ULOC_ACTUAL_LOCALE,
+	  *                    ULOC_VALID_LOCALE. By default the actual
+	  *                    locale is returned.
+	  * @return string     the locale of this formatter  or 'false' if error
+	 */
+
+	 public function datefmt_get_locale($fmt , $type = ULOC_ACTUAL_LOCALE) {}
+
+         /**
+	  * @return string ID string for the time zone used by this formatter
+	  */
+	 public function datefmt_get_timezone_id($fmt) {}
+
+
+	/**
+	 * sets the time zone to use
+	 * @param string $zone zone ID string of the time zone to use.
+	 *                     if null or the empty string, the default time zone for
+	 *                     the runtime is used.
+	 * @return boolean 'true' on successful setting of the time zone, 'false'
+	 *                 if an error occurred (such as the time zone wasn't recognized).
+	 */
+        public function datefmt_set_timezone_id($fmt , $zone) {}
+
+        /**
+         * sets the calendar used to the appropriate calendar, which must be 
+	 * one of the constants defined above. Some examples include:
+         *   - Gregorian calendar
+         *   - Traditional
+         * Default value is GREGORIAN
+	 * @param integer $which the calendar (an enumerated constant) to use.
+         * @return boolean 'true' if successful, 'false' if an error occurred or if the calendar was not recognized 
+	 */
+         public function datefmt_set_calendar($fmt , $which) {}
+
+        /**
+	 * @return integer the calendar being used by the formatter
+	 */
+         public function datefmt_get_calendar($fmt) {}
+
+        /**
+         * @return string the pattern string being used to format/parse
+         */
+        public function  datefmt_get_pattern($fmt) {}
+
+        /**
+         * @param  string $pattern new pattern string to use
+         * @return boolean 'true' if successful, 'false' if an error occured. Bad format
+         *                 strings are usually the cause of the latter.
+         */
+        public function datefmt_set_pattern($fmt , $pattern) {}
+
+
+	/**
+	 * Get the error code from last operation
+	 *
+	 * Returns error code from the last number formatting operation.
+	 *
+	 * @return integer the error code, one of UErrorCode values. Initial value is U_ZERO_ERROR.
+	 */
+	public function datefmt_get_error_code($fmt) {}
+	/**
+	 * Get the error text from the last operation.
+	 *
+	 * @return string Description of the last error.
+	 */
+	public function datefmt_get_error_message($fmt) {}
+
 
 
 ?>
